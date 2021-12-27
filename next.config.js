62de379eb9151a88ebd22f41c340da405ee39c79
박준민 <Jade@cnspartner.com>
2021-12-27 16:17:16 +0900

    기본설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config, { webpack }) {
+    config.resolve = {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+      ...config.resolve,
+    };
+    return config;
+  },
+};

879476c8214802ffa835e97b0135613e783c4455
박준민 <jjun0214z@gmail.com>
2021-12-29 17:12:50 +0900

    global style 설정

---
+++ src/assets/styles/index.ts
@@ -0,0 +1,4 @@
+import STYLED_PC from './pc';
+import STYLED_MOBILE from './mobile';
+
+export { STYLED_PC, STYLED_MOBILE };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import type { AppProps } from "next/app";
+import GlobalStyle from '@/assets/styles/globalStyles';
+import { theme } from '@/assets/styles/theme';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
